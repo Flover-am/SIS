dc897ac3d545c63ac942d8af65c1f93e74529813
flover.am-powershall <211250152@smail.nju.edu.cn>
2024-03-06 18:44:24 +0800

    feat: test dir

--- Deploy/deploy.sh
+++ Deploy/deploy.sh
@@ -2,6 +2,9 @@
 version=1.0.0
 docker build -t prism036-backend:$version .
 echo "prism036-backend:$version is built successfully!"
+pwd
+whoami
+tree
 # 允许prism036-backend不存在而报错
 docker stop prism036-backend || true
 docker rm prism036-backend || true
