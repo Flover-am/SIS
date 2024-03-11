f3f8fc2de5e2ad57fea327198680c262b6bc0931
Rashawn <ruichenxue@smail.nju.edu.cn>
2024-03-11 20:45:13 +0800

    feat: 添加sql脚本

---
+++ sql/t_auth_user.sql
@@ -0,0 +1,13 @@
+create table prism063.t_auth_user
+(
+    id          bigint unsigned auto_increment comment '用户id'
+        primary key,
+    username    varchar(20)  not null comment '用户名',
+    password    varchar(200) not null comment '用户密码',
+    create_time datetime     not null comment '数据创建时间',
+    update_time datetime     not null comment '数据修改时间',
+    constraint uk_username
+        unique (username)
+)
+    comment '用户表';
+
