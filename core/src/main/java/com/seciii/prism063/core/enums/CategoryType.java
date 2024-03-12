a91c7041252319da22f0cfb8ab873565a54e997c
mas_w <211250143@smail.nju.edu.cn>
2024-03-12 19:48:24 +0800

    fix:修正CategoryType的字符串常量以及注释

--- core/src/main/java/com/seciii/prism063/core/enums/CategoryType.java
+++ core/src/main/java/com/seciii/prism063/core/enums/CategoryType.java
@@ -3,6 +3,7 @@ package com.seciii.prism063.core.enums;
 
 /**
  * 类型枚举类，迭代二将根据需求重构
+ *
  * @author wangmingsong
  * @date 2024.03.01
  */
@@ -10,53 +11,73 @@ public enum CategoryType {
     /**
      * 新闻分类
      */
-    DOMESTIC("国内","domestic"),
-    INTERNATIONAL("国际","international"),
-    SOCIETY("社会","society"),
-    SPORTS("体育","sports"),
-    ENTERTAINMENT("娱乐","entertainment"),
-    TECHNOLOGY("科技","technology"),
-    FINANCE("财经","finance"),
-    STOCK("股票","stock"),
-    AMERICAN_STOCK("美股","american stock"),
-    OTHER("其他","other");
+    DOMESTIC("国内", "domestic"),
+    INTERNATIONAL("国际", "international"),
+    SOCIETY("社会", "society"),
+    SPORTS("体育", "sports"),
+    ENTERTAINMENT("娱乐", "entertainment"),
+    TECHNOLOGY("科技", "technology"),
+    FINANCE("财经", "finance"),
+    STOCK("股票", "stock"),
+    AMERICAN_STOCK("美股", "American stock"),
+    OTHER("其他", "other");
 
     private final String categoryCN;
     private final String categoryEN;
 
-    CategoryType(String categoryCN,String categoryEN) {
-        this.categoryCN=categoryCN;
-        this.categoryEN=categoryEN;
+    CategoryType(String categoryCN, String categoryEN) {
+        this.categoryCN = categoryCN;
+        this.categoryEN = categoryEN;
     }
-    public String getCategoryCN(){
+
+    public String getCategoryCN() {
         return this.categoryCN;
     }
-    public String getCategoryEN(){
+
+    public String getCategoryEN() {
         return this.categoryEN;
     }
-    public int toInt(){
+
+    public int toInt() {
         return this.ordinal();
     }
-    public boolean equals(int ord){
-        return this.ordinal()==ord;
+
+    public boolean equals(int ord) {
+        return this.ordinal() == ord;
     }
-    public boolean equals(String category){
-        return this.categoryCN.equals(category)||this.categoryEN.equals(category);
+
+    public boolean equals(String category) {
+        return this.categoryCN.equals(category) || this.categoryEN.equals(category);
     }
-    public String toString(){
+
+    public String toString() {
         return this.categoryEN;
     }
-    public static CategoryType getCategoryType(String category){
-        for(CategoryType categoryType:CategoryType.values()){
-            if(categoryType.getCategoryCN().equals(category)||categoryType.getCategoryEN().equals(category)){
+
+    /**
+     * 从字符串获取新闻类型
+     *
+     * @param category 类型字符串
+     * @return 新闻类型
+     */
+    public static CategoryType getCategoryType(String category) {
+        for (CategoryType categoryType : CategoryType.values()) {
+            if (categoryType.getCategoryCN().equals(category) || categoryType.getCategoryEN().equals(category)) {
                 return categoryType;
             }
         }
         return CategoryType.OTHER;
     }
-    public static CategoryType getCategoryType(int category){
-        for(CategoryType categoryType:CategoryType.values()){
-            if(categoryType.ordinal()==category){
+
+    /**
+     * 从新闻类型下标获取新闻类型
+     *
+     * @param category 类型下标
+     * @return 新闻类型
+     */
+    public static CategoryType getCategoryType(int category) {
+        for (CategoryType categoryType : CategoryType.values()) {
+            if (categoryType.ordinal() == category) {
                 return categoryType;
             }
         }
