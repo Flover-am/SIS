e33f1d03109fd7469d9ff22b9c6668baff43d269
wang mingsong <211250143@smail.nju.edu.cn>
2024-03-27 23:40:20 +0800

    fix: 优化依赖；删除冗余注释

--- core/src/main/java/com/seciii/prism030/core/config/AOPConfig.java
+++ core/src/main/java/com/seciii/prism030/core/config/AOPConfig.java
@@ -1,18 +1,10 @@
 package com.seciii.prism030.core.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
 public class AOPConfig {
-    // 请在此处定义切面Bean
-    // 例如：
-    // @Bean
-    // public LogAspect logAspect() {
-    //     return new LogAspect();
-    // }
-
-
-
 }
